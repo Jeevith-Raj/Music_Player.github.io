76b54af7bfc01242aa99142f538b55b01995ce57
Jeevith-Raj <20uad028@kamarajengg.edu.in>
2024-05-08 23:22:59 -0400

    this is also a commit

--- script.js
+++ script.js
@@ -8,5 +8,28 @@ song.onloadedmetadata = function(){
 }
 
 function playpause(){
-    if(ctrlicon.classList.contains("f"))
+    if(ctrlicon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlicon.classList.add("fa-pause");
+        ctrlicon.classList.remove("fa-play");
+
+    }
+    else{
+        song.play();
+        ctrlicon.classList.add("fa-pause");
+        ctrlicon.classList.remove("fa-play");
+    }
+}
+
+if(song.play()){
+    setInterval(()=>{
+        progress.value = song.currentTime;
+    },500);   
+}
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    ctrlicon.classList.add("fa-pause");
+    ctrlicon.classList.remove("fa-play");
+    
 }
